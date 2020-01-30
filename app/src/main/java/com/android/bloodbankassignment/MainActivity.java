7b715276ddbf2fba48f4fd27bdc42314743cddf6
john doe <nikaghimire0@gmail.com>
2020-01-30 19:50:53 +0545

    signup

--- app/src/main/java/com/android/bloodbankassignment/MainActivity.java
+++ app/src/main/java/com/android/bloodbankassignment/MainActivity.java
@@ -1,134 +1,155 @@
 package com.android.bloodbankassignment;
-
-import android.app.Activity;
-
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
-
-import android.os.Bundle;
-
-import androidx.annotation.Nullable;
-import androidx.annotation.StringRes;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.view.KeyEvent;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.android.bloodbankassignment.R;
-import com.android.bloodbankassignment.ui.login.LoggedInUserView;
-import com.android.bloodbankassignment.ui.login.LoginFormState;
-import com.android.bloodbankassignment.ui.login.LoginResult;
-import com.android.bloodbankassignment.ui.login.LoginViewModel;
-import com.android.bloodbankassignment.ui.login.LoginViewModelFactory;
+import com.android.bloodbankassignment.model.Login;
+import com.example.facebook.model.Response;
+import com.example.facebook.model.User;
+import com.example.facebook.network.Retrofit;
+import com.example.facebook.network.RetrofitApi;
+import com.example.facebook.utils.Constants;
+import com.example.facebook.utils.Validator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
 
 public class MainActivity extends AppCompatActivity {
 
-    private LoginViewModel loginViewModel;
+    EditText emailtxt,passwordtxt;
+    Button loginbtn, regbtn;
+    TextView forgot;
+    List<Response> loginres = new ArrayList<>();
+    public static List<User> userlist = new ArrayList<>();
+    SharedPreferences sharedPreferences;
+    private static final String TAG = "MainActivity";
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        loginViewModel = ViewModelProviders.of(this, new LoginViewModelFactory())
-                .get(LoginViewModel.class);
 
-        final EditText usernameEditText = findViewById(R.id.username);
-        final EditText passwordEditText = findViewById(R.id.password);
-        final Button loginButton = findViewById(R.id.login);
-        final ProgressBar loadingProgressBar = findViewById(R.id.loading);
+        viewsInit();
 
-        loginViewModel.getLoginFormState().observe(this, new Observer<LoginFormState>() {
-            @Override
-            public void onChanged(@Nullable LoginFormState loginFormState) {
-                if (loginFormState == null) {
-                    return;
-                }
-                loginButton.setEnabled(loginFormState.isDataValid());
-                if (loginFormState.getUsernameError() != null) {
-                    usernameEditText.setError(getString(loginFormState.getUsernameError()));
-                }
-                if (loginFormState.getPasswordError() != null) {
-                    passwordEditText.setError(getString(loginFormState.getPasswordError()));
-                }
-            }
-        });
 
-        loginViewModel.getLoginResult().observe(this, new Observer<LoginResult>() {
-            @Override
-            public void onChanged(@Nullable LoginResult loginResult) {
-                if (loginResult == null) {
-                    return;
-                }
-                loadingProgressBar.setVisibility(View.GONE);
-                if (loginResult.getError() != null) {
-                    showLoginFailed(loginResult.getError());
-                }
-                if (loginResult.getSuccess() != null) {
-                    updateUiWithUser(loginResult.getSuccess());
-                }
-                setResult(Activity.RESULT_OK);
+        sharedPreferences = getSharedPreferences("login", MODE_PRIVATE);
 
-                //Complete and destroy login activity once successful
-                finish();
-            }
-        });
+    }
 
-        TextWatcher afterTextChangedListener = new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                // ignore
-            }
+    private void viewsInit(){
 
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                // ignore
-            }
+        emailtxt = findViewById(R.id.emailtxt);
+        passwordtxt = findViewById(R.id.passwordtxt);
+        loginbtn = findViewById(R.id.loginbtn);
+        regbtn = findViewById(R.id.createacc);
+        forgot = findViewById(R.id.forgot);
 
-            @Override
-            public void afterTextChanged(Editable s) {
-                loginViewModel.loginDataChanged(usernameEditText.getText().toString(),
-                        passwordEditText.getText().toString());
-            }
-        };
-        usernameEditText.addTextChangedListener(afterTextChangedListener);
-        passwordEditText.addTextChangedListener(afterTextChangedListener);
-        passwordEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if (actionId == EditorInfo.IME_ACTION_DONE) {
-                    loginViewModel.login(usernameEditText.getText().toString(),
-                            passwordEditText.getText().toString());
-                }
-                return false;
-            }
-        });
-
-        loginButton.setOnClickListener(new View.OnClickListener() {
+        loginbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                loadingProgressBar.setVisibility(View.VISIBLE);
-                loginViewModel.login(usernameEditText.getText().toString(),
-                        passwordEditText.getText().toString());
+                validate();
+
             }
         });
+        regbtn.setOnClickListener(new View.OnClickListener() {
+                                      @Override
+                                      public void onClick(View v) {
+                                          startActivity(new Intent(MainActivity.this, RegisterActivity.class));
+
+                                      }
+                                  }
+        );
     }
 
-    private void updateUiWithUser(LoggedInUserView model) {
-        String welcome = getString(R.string.welcome) + model.getDisplayName();
-        // TODO : initiate successful logged in experience
-        Toast.makeText(getApplicationContext(), welcome, Toast.LENGTH_LONG).show();
+    private void validate(){
+        int err = 0;
+        String email = emailtxt.getText().toString();
+        String password = passwordtxt.getText().toString();
+
+        if(!Validator.validateEmail(email) ){
+            err++;
+            emailtxt.requestFocus();
+            emailtxt.setError("Email Cannot Be Empty!");
+            emailtxt.setText(null);
+        }
+
+        if(!Validator.validateFields(password)){
+            err++;
+            passwordtxt.requestFocus();
+            passwordtxt.setError("Password Cannoot Be Empty!");
+            passwordtxt.setText(null);
+        }
+
+        if(err == 0){
+            login();
+        }
     }
 
-    private void showLoginFailed(@StringRes Integer errorString) {
-        Toast.makeText(getApplicationContext(), errorString, Toast.LENGTH_SHORT).show();
+    private void setError(){
+        emailtxt.setText(null);
+        passwordtxt.setText(null);
+        emailtxt.requestFocus();
+        emailtxt.setError("Incorrect email or password!");
     }
+
+    private void login(){
+
+        String email = emailtxt.getText().toString();
+        String password = passwordtxt.getText().toString();
+        RetrofitApi api =  Retrofit.retrofitInit().create(RetrofitApi.class);
+        Call<List<Response>> loginresponse = api.login(new Login(email, password));
+
+        final SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        loginresponse.enqueue(new Callback<List<Response>>() {
+            @Override
+            public void onResponse(Call<List<Response>> call, retrofit2.Response<List<Response>> response) {
+
+
+                if(response.isSuccessful()){
+                    loginres = response.body();
+                    userlist.add(loginres.get(0).getUser());
+                    editor.putString(Constants.TOKEN, loginres.get(0).getToken());
+                    editor.putString(Constants.EMAIL, userlist.get(0).getEmail());
+
+                    editor.commit();
+
+                    Intent intent = new Intent(MainActivity.this, DashboardActivity.class);
+                    startActivity(intent);
+                    finish();
+
+//                    Toast.makeText(MainActivity.this, sharedPreferences.getString(Constants.TOKEN, null), Toast.LENGTH_SHORT).show();
+
+
+                }else{
+                    setError();
+//                    Toast.makeText(MainActivity.this, "Fail", Toast.LENGTH_SHORT).show();
+                }
+
+
+            }
+
+            @Override
+            public void onFailure(Call<List<Response>> call, Throwable t) {
+                Log.d(TAG, "onFailure: " + t.toString());
+            }
+        });
+
+
+
+    }
+
+
+
 }

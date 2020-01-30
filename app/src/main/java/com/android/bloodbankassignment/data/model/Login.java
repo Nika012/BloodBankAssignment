e670494f42957b86d18d0fa526bd19e1bc0f1057
john doe <nikaghimire0@gmail.com>
2020-01-30 23:20:05 +0545

    Design Update

---
+++ app/src/main/java/com/android/bloodbankassignment/data/model/Login.java
@@ -0,0 +1,23 @@
+package com.android.bloodbankassignment.data.model;
+
+public class Login {
+
+
+        private String email;
+        private String password;
+
+        public Login(String email, String password) {
+            this.email = email;
+            this.password = password;
+
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+    }
+
